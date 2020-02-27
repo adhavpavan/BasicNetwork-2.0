79eff775be59b3233020c0b272525a0b57c20369
adhavpavan <pavan@infinichains.com>
2020-02-27 23:28:19 +0530

    create channel and deploy chaincode scripts added

--- artifacts/channel/create-artifacts.sh
+++ artifacts/channel/create-artifacts.sh
@@ -24,3 +24,9 @@ configtxgen -profile OrdererGenesis -channelID $SYS_CHANNEL  -outputBlock ./gene
 
 # Generate channel configuration block
 configtxgen -profile BasicChannel -outputCreateChannelTx ./mychannel.tx -channelID $CHANNEL_NAME
+
+echo "#######    Generating anchor peer update for Org1MSP  ##########"
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+
+echo "#######    Generating anchor peer update for Org2MSP  ##########"
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
