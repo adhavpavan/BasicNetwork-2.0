a1a1437d2c51d81706a26e0146506e10ddee6b47
adhavpavan <pavan@infinichains.com>
2020-03-02 00:38:48 +0530

    Create channel working script added

---
+++ cleanup.sh
@@ -0,0 +1,3 @@
+docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
+
+docker rmi $(docker iamges | grep dev)
--- createChannel.sh
+++ createChannel.sh
@@ -9,73 +9,75 @@ echo $FABRIC_CFG_PATH
 
 export CHANNEL_NAME=mychannel
 
-# setGlobalsForOrderer(){
-#     export CORE_PEER_LOCALMSPID="OrdererMSP"
-#     export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
-#     export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/example.com/users/Admin@example.com/msp
+setGlobalsForOrderer(){
+    export CORE_PEER_LOCALMSPID="OrdererMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/example.com/users/Admin@example.com/msp
     
-# }
+}
 
 setGlobalsForPeer0Org1(){
     export CORE_PEER_LOCALMSPID="Org1MSP"  
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-    export CORE_PEER_ADDRESS=peer0.org1.example.com:7051
+    export CORE_PEER_ADDRESS=localhost:7051
 }
 
-# setGlobalsForPeer1Org1(){
-#     export CORE_PEER_LOCALMSPID="Org1MSP"
-#     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
-#     export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-#     export CORE_PEER_ADDRESS=peer1.org1.example.com:8051
+setGlobalsForPeer1Org1(){
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:8051
     
-# }
+}
 
-# setGlobalsForPeer0Org2(){
-#     export CORE_PEER_LOCALMSPID="Org2MSP"
-#     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
-#     export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-#     export CORE_PEER_ADDRESS=peer0.org2.example.com:9051
+setGlobalsForPeer0Org2(){
+    export CORE_PEER_LOCALMSPID="Org2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:9051
     
-# }
+}
 
-# setGlobalsForPeer1Org2(){
-#     export CORE_PEER_LOCALMSPID="Org2MSP"
-#     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
-#     export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-#     export CORE_PEER_ADDRESS=peer1.org2.example.com:10051
+setGlobalsForPeer1Org2(){
+    export CORE_PEER_LOCALMSPID="Org2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:10051
     
-# }
+}
 
 createChannel(){
-    # setGlobalsForPeer0Org1()
+    setGlobalsForPeer0Org1
 
-    peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME --ordererTLSHostnameOverride orderer.example.com -f ./artifacts/channel/${CHANNEL_NAME}.tx --outputBlock ./channel-artifacts/${CHANNEL_NAME}.block --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+    peer channel create -o localhost:7050 -c $CHANNEL_NAME --ordererTLSHostnameOverride orderer.example.com -f ./artifacts/channel/${CHANNEL_NAME}.tx --outputBlock ./channel-artifacts/${CHANNEL_NAME}.block --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+}
+
+
+joinChannel(){
+    setGlobalsForPeer0Org1
+    peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    
+    setGlobalsForPeer1Org1
+    peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    
+    setGlobalsForPeer0Org2
+    peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    
+    setGlobalsForPeer1Org2
+    peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
+    
+}
+
+updateAnchorPeers(){
+    setGlobalsForPeer0Org1
+    peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+    
+    setGlobalsForPeer0Org2
+    peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+    
 }
 
 createChannel
-
-# joinChannel(){
-#     setGlobalsForPeer0Org1()
-#     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
-    
-#     setGlobalsForPeer1Org1()
-#     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
-    
-#     setGlobalsForPeer0Org2()
-#     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
-    
-#     setGlobalsForPeer0Org2()
-#     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
-    
-    
-# }
-
-# updateAnchorPeers(){
-#     setGlobalsForPeer0Org1()
-#     peer channel update -o orderer.example.com:7050 :7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
-    
-#     setGlobalsForPeer0Org2()
-#     peer channel update -o orderer.example.com:7050 :7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
-    
-# }
+joinChannel
+updateAnchorPeers
