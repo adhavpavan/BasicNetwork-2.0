450d1b65a265e129b2fd24d2d66b2dbcbdde2e32
adhavpavan <pavan@infinichains.com>
2020-04-21 09:21:38 +0530

    API added for registering and enrolling user using new fabric network client

---
+++ api-2.0/app.js
@@ -0,0 +1,108 @@
+'use strict';
+const log4js = require('log4js');
+const logger = log4js.getLogger('BasicNetwork');
+const bodyParser = require('body-parser');
+const http = require('http')
+const util = require('util');
+const express = require('express')
+const app = express();
+const expressJWT = require('express-jwt');
+const jwt = require('jsonwebtoken');
+const bearerToken = require('express-bearer-token');
+const cors = require('cors');
+const constants = require('./config/constants.json')
+
+const host = process.env.HOST || constants.host;
+const port = process.env.PORT || constants.port;
+
+
+const helper = require('./app/helper')
+
+app.options('*', cors());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+// set secret variable
+app.set('secret', 'thisismysecret');
+app.use(expressJWT({
+    secret: 'thisismysecret'
+}).unless({
+    path: ['/users']
+}));
+app.use(bearerToken());
+
+
+app.use((req, res, next) => {
+    logger.debug('New req for %s', req.originalUrl);
+    if (req.originalUrl.indexOf('/users') >= 0) {
+        return next();
+    }
+    var token = req.token;
+    jwt.verify(token, app.get('secret'), (err, decoded) => {
+        if (err) {
+            res.send({
+                success: false,
+                message: 'Failed to authenticate token. Make sure to include the ' +
+                    'token returned from /users call in the authorization header ' +
+                    ' as a Bearer token'
+            });
+            return;
+        } else {
+            req.username = decoded.username;
+            req.orgname = decoded.orgName;
+            logger.debug(util.format('Decoded from JWT token: username - %s, orgname - %s', decoded.username, decoded.orgName));
+            return next();
+        }
+    });
+});
+
+var server = http.createServer(app).listen(port, function () { console.log(`app is listening on ${port}`) });
+logger.info('****************** SERVER STARTED ************************');
+logger.info('***************  http://%s:%s  ******************', host, port);
+server.timeout = 240000;
+
+function getErrorMessage(field) {
+    var response = {
+        success: false,
+        message: field + ' field is missing or Invalid in the request'
+    };
+    return response;
+}
+
+// Register and enroll user
+app.post('/users', async function (req, res) {
+    var username = req.body.username;
+    var orgName = req.body.orgName;
+    logger.debug('End point : /users');
+    logger.debug('User name : ' + username);
+    logger.debug('Org name  : ' + orgName);
+    if (!username) {
+        res.json(getErrorMessage('\'username\''));
+        return;
+    }
+    if (!orgName) {
+        res.json(getErrorMessage('\'orgName\''));
+        return;
+    }
+
+    var token = jwt.sign({
+        exp: Math.floor(Date.now() / 1000) + parseInt(constants.jwt_expiretime),
+        username: username,
+        orgName: orgName
+    }, app.get('secret'));
+
+    let response = await helper.getRegisteredUser(username, orgName, true);
+
+    logger.debug('-- returned from registering the username %s for organization %s', username, orgName);
+    if (response && typeof response !== 'string') {
+        logger.debug('Successfully registered the username %s for organization %s', username, orgName);
+        response.token = token;
+        res.json(response);
+    } else {
+        logger.debug('Failed to register the username %s for organization %s with::%s', username, orgName, response);
+        res.json({ success: false, message: response });
+    }
+
+});
---
+++ api-2.0/app/helper.js
@@ -0,0 +1,113 @@
+'use strict';
+
+var { Gateway, Wallets } = require('fabric-network');
+const path = require('path');
+const FabricCAServices = require('fabric-ca-client');
+const fs = require('fs');
+
+const util = require('util');
+
+
+const ccpPath = path.resolve(__dirname, '..', 'config', 'connection-org1.json');
+const ccpJSON = fs.readFileSync(ccpPath, 'utf8')
+const ccp = JSON.parse(ccpJSON);
+
+
+const getRegisteredUser = async (username, userOrg, isJson) => {
+
+    console.log(JSON.stringify(Wallets))
+
+    // Create a new CA client for interacting with the CA.
+    const caURL = ccp.certificateAuthorities['ca.org1.example.com'].url;
+    const ca = new FabricCAServices(caURL);
+
+    const walletPath = path.join(process.cwd(), 'wallet');
+    const wallet = await Wallets.newFileSystemWallet(walletPath);
+    console.log(`Wallet path: ${walletPath}`);
+
+    const userIdentity = await wallet.get(username);
+    if (userIdentity) {
+        console.log('An identity for the user "user1" already exists in the wallet');
+        return;
+    }
+
+    // Check to see if we've already enrolled the admin user.
+    const adminIdentity = await wallet.get('admin');
+    if (!adminIdentity) {
+        console.log('An identity for the admin user "admin" does not exist in the wallet');
+        await enrollAdmin();
+        adminIdentity = await wallet.get('admin');
+        console.log("Admin Enrolled Successfully")
+    }
+
+     // build a user object for authenticating with the CA
+     const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
+     const adminUser = await provider.getUserContext(adminIdentity, 'admin');
+
+ // Register the user, enroll the user, and import the new identity into the wallet.
+    const secret = await ca.register({ affiliation: 'org1.department1', enrollmentID: username, role: 'client' }, adminUser);
+    const enrollment = await ca.enroll({ enrollmentID: username, enrollmentSecret: secret });
+    const x509Identity = {
+        credentials: {
+            certificate: enrollment.certificate,
+            privateKey: enrollment.key.toBytes(),
+        },
+        mspId: 'Org1MSP',
+        type: 'X.509',
+    };
+
+    await wallet.put(username, x509Identity);
+    console.log('Successfully registered and enrolled admin user "user1" and imported it into the wallet');
+
+    var response = {
+        success: true,
+        message: username + ' enrolled Successfully',
+    };
+    return response
+}
+
+
+const enrollAdmin = async () => {
+
+    console.log('calling enroll Admin method')
+
+    try {
+
+        const caInfo = ccp.certificateAuthorities['ca.org1.example.com'];
+        const caTLSCACerts = caInfo.tlsCACerts.pem;
+        const ca = new FabricCAServices(caInfo.url, { trustedRoots: caTLSCACerts, verify: false }, caInfo.caName);
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the admin user.
+        const identity = await wallet.get('admin');
+        if (identity) {
+            console.log('An identity for the admin user "admin" already exists in the wallet');
+            return;
+        }
+
+        // Enroll the admin user, and import the new identity into the wallet.
+        const enrollment = await ca.enroll({ enrollmentID: 'admin', enrollmentSecret: 'adminpw' });
+        const x509Identity = {
+            credentials: {
+                certificate: enrollment.certificate,
+                privateKey: enrollment.key.toBytes(),
+            },
+            mspId: 'Org1MSP',
+            type: 'X.509',
+        };
+        await wallet.put('admin', x509Identity);
+        console.log('Successfully enrolled admin user "admin" and imported it into the wallet');
+
+    } catch (error) {
+        console.error(`Failed to enroll admin user "admin": ${error}`);
+    }
+
+
+}
+
+
+exports.getRegisteredUser = getRegisteredUser
