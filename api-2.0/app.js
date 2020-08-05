84c5d81b8c5ca393b00ddb15f97bb9863428103f
adhavpavan <pavan@infinichains.com>
2020-08-05 23:25:15 +0530

    query import added in app.js

--- api-2.0/app.js
+++ api-2.0/app.js
@@ -19,6 +19,7 @@ const port = process.env.PORT || constants.port;
 const helper = require('./app/helper')
 const invoke = require('./app/invoke')
 const qscc = require('./app/qscc')
+const query = require('./app/query')
 
 app.options('*', cors());
 app.use(cors());
@@ -111,6 +112,43 @@ app.post('/users', async function (req, res) {
     }
 
 });
+
+// Login and get jwt
+app.post('/users/login', async function (req, res) {
+    var username = req.body.username;
+    var orgName = req.body.orgName;
+    logger.debug('End point : /users');
+    logger.debug('User name : ' + username);
+    logger.debug('Org name  : ' + orgName);
+    if (!username) {
+        res.json(getErrorMessage('\'username\''));
+        return;
+    }
+    if (!orgName) {
+        res.json(getErrorMessage('\'orgName\''));
+        return;
+    }
+
+    var token = jwt.sign({
+        exp: Math.floor(Date.now() / 1000) + parseInt(constants.jwt_expiretime),
+        username: username,
+        orgName: orgName
+    }, app.get('secret'));
+
+    let response = await helper.getRegisteredUser(username, orgName, true);
+
+    logger.debug('-- returned from registering the username %s for organization %s', username, orgName);
+    if (response && typeof response !== 'string') {
+        logger.debug('Successfully registered the username %s for organization %s', username, orgName);
+        response.token = token;
+        res.json(response);
+    } else {
+        logger.debug('Failed to register the username %s for organization %s with::%s', username, orgName, response);
+        res.json({ success: false, message: response });
+    }
+
+});
+
 
 // Invoke transaction on chaincode on target peers
 app.post('/channels/:channelName/chaincodes/:chaincodeName', async function (req, res) {
